a25c6cd9267b7252ad2d5e973b6d1da1370ab1f4
Wiki <wiikiiki@outlook.com>
2022-08-23 17:03:43 +0800

    change app to todolist

---
+++ src/TodoList.js
@@ -0,0 +1,19 @@
+import React, { Component, Fragment } from "react";
+
+class TodoList extends Component {
+  render() {
+    return(
+      <Fragment>
+        <div>
+          <input /><button>提交</button>
+          <ul>
+            <li>task 1</li>
+            <li>task 2</li>
+          </ul>
+        </div>
+      </Fragment>
+    )
+}
+}
+
+export default TodoList;
