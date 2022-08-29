edce31672120ab3734e6be1ba79baeda822d058a
Wiikiiki <wiikiiki@outlook.com>
2022-08-29 16:09:30 +0800

    task deleted

--- src/TodoList.js
+++ src/TodoList.js
@@ -2,19 +2,19 @@ import React, { Component, Fragment } from "react";
 
 class TodoList extends Component {
   render() {
-    return(
+    return (
       <Fragment>
         <div>
-          <input /><button>提交</button>
+          <input />
+          <button>提交</button>
           <ul>
             <li>task 1</li>
             <li>task 2</li>
-            <li>task 3</li>
           </ul>
         </div>
       </Fragment>
-    )
-}
+    );
+  }
 }
 
 export default TodoList;
